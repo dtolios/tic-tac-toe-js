3fb735f03c485ed702c7f29fcc9ac8b890f57c3f
Dimitri Tolios <dtolios@riotgames.com>
2017-04-05 17:55:39 -0700

    First pass trying to set up the game and turn one

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const Game = (function () {
+
+    const $body = $('body');
+    const $board = $('#board');
+    const $startScreen = $('<div class="screen screen-start" id="start"><header><h1>Tic Tac Toe</h1><a href="#" class="button">Start game</a></header></div>');
+
+    function startGame() {
+        $body.append($startScreen);
+        const $button = $('.button');
+        $button.on('click', () => {
+            $startScreen.remove();
+            appendBoard();
+            const startingPlayer = getStartingPlayer();
+            $(`#${startingPlayer}`).addClass('active');
+            takeTurn(startingPlayer);
+        });
+    }
+
+    function appendBoard() {
+        $board.appendTo($body);
+    }
+
+    function removeBoard() {
+        $board.remove();
+    }
+
+    function getStartingPlayer() {
+        const coin = Math.floor(Math.random() * 2);
+        if (coin === 1)
+            return 'player1';
+        else
+            return 'player2';
+    }
+
+    function takeTurn(currPlayer) {
+    let symbol = '';
+    if (currPlayer === 'player1') {
+        symbol = 'img/o.svg';
+    }
+    else {
+        symbol = 'img/x.svg';
+    }
+
+    $('.box').hover(
+        () => {
+            console.log('hey');
+            $(this).css('background-image', 'url(img/o.svg) no-repeat');
+        },
+        () => {
+            $(this).css('background-image', 'initial');
+        }
+    );
+}
+
+    function init() {
+        removeBoard();
+        startGame();
+    }
+
+    return {
+        init: init
+    }
+})();
+
+Game.init();
