e7476ff8d3268934d10b1f439263581a200ecdb6
Dimitri Tolios <dtolios@riotgames.com>
2017-04-12 18:04:25 -0700

    Not currently working because of click event for taking turn, but did a lot of work

--- js/script.js
+++ js/script.js
@@ -2,20 +2,100 @@ const Game = (function () {
 
     const $body = $('body');
     const $board = $('#board');
+    const $box = $('.box');
     const $startScreen = $('<div class="screen screen-start" id="start"><header><h1>Tic Tac Toe</h1><a href="#" class="button">Start game</a></header></div>');
+    const $endScreen = $('<div class="screen screen-win" id="finish"><header><h1>Tic Tac Toe</h1><p class="message"></p><a href="#" class="button">New game</a></header></div>');
+    let winner = 'none';
+    const boardState = ['NaN','NaN','NaN','NaN','NaN','NaN','NaN','NaN','NaN'];
+
+
+    // GAME START and GAME LOOP //
+    function init() {
+        removeBoard();
+        startGame();
+    }
 
     function startGame() {
-        $body.append($startScreen);
+
+        appendStartScreen();
         const $button = $('.button');
+
         $button.on('click', () => {
-            $startScreen.remove();
+            removeStartScreen();
             appendBoard();
             const startingPlayer = getStartingPlayer();
-            $(`#${startingPlayer}`).addClass('active');
-            takeTurn(startingPlayer);
+            playGame(startingPlayer);
+        });
+
+    }
+
+    function playGame(player) {
+        let turn = 1;
+        $(`#${player}`).addClass('active');
+        while(turn !== 9) {
+            $box.on('click', takeTurn(player, event));
+            turn += 1;
+            if(player === 'player1')
+                player = 'player2';
+            else
+                player = 'player1';
+        }
+        endGame(winner);
+    }
+
+    function endGame(winner) {
+        removeBoard();
+        appendEndScreen();
+
+        if(winner === 'player1') {
+            $('#finish').addClass('screen-win-one');
+            $('.message').text('Winner');
+        }
+        else {
+            $('#finish').addClass('screen-win-tie');
+            $('.message').text('It\'s a Tie!');
+        }
+
+        const $sbutton = $('.button');
+
+        $sbutton.on('click', () => {
+            removeEndScreen();
+            appendBoard();
+            const newStartingPlayer = getStartingPlayer();
+            playGame(newStartingPlayer);
         });
     }
 
+    function takeTurn(currPlayer, event) {
+        let symbol = '';
+        if (currPlayer === 'player1') {
+            symbol = 'img/o.svg';
+        }
+        else {
+            symbol = 'img/x.svg';
+        }
+
+        $box.hover(
+            (ev) => {
+                $(ev.target).css('background-image', `url(${symbol})`);
+                $(ev.target).css('background-size', `100px 100px`);
+            },
+            (ev) => {
+                $(ev.target).css('background-image', 'initial');
+            }
+        );
+
+        if(currPlayer === 'player1') {
+            $(event.target).addClass('box-filled-1');
+            $(event.target).off();
+        }
+        else {
+            $(event.target).addClass('box-filled-2');
+            $(event.target).off();
+        }
+    }
+
+    // RENDER FUNCTIONS //
     function appendBoard() {
         $board.appendTo($body);
     }
@@ -24,6 +104,23 @@ const Game = (function () {
         $board.remove();
     }
 
+    function appendStartScreen() {
+        $startScreen.appendTo($body);
+    }
+
+    function removeStartScreen() {
+        $startScreen.remove();
+    }
+
+    function appendEndScreen() {
+        $endScreen.appendTo($body);
+    }
+
+    function removeEndScreen() {
+        $endScreen.remove();
+    }
+
+    // HELPER FUNCTIONS //
     function getStartingPlayer() {
         const coin = Math.floor(Math.random() * 2);
         if (coin === 1)
@@ -32,45 +129,16 @@ const Game = (function () {
             return 'player2';
     }
 
-    function takeTurn(currPlayer) {
-    let symbol = '';
-    if (currPlayer === 'player1') {
-        symbol = 'img/o.svg';
-    }
-    else {
-        symbol = 'img/x.svg';
+    function hasWon() {
+        winner = 'player1';
+        return false;
     }
 
-    $('.box').hover(
-        (ev) => {
-            $(ev.target).css('background-image', `url(${symbol})`);
-            $(ev.target).css('background-size', `100px 100px`);
-        },
-        (ev) => {
-            $(ev.target).css('background-image', 'initial');
-        }
-    );
-
-    $('.box').on('click', (ev) => {
-        if(currPlayer === 'player1') {
-            $(ev.target).addClass('box-filled-1');
-            $(ev.target).off();
-        }
-        else {
-            $(ev.target).addClass('box-filled-2');
-            $(ev.target).off();
-        }
-    });
-}
-
-    function init() {
-        removeBoard();
-        startGame();
-    }
-
+    // PUBLIC API //
     return {
         init: init
     }
+
 })();
 
 Game.init();
